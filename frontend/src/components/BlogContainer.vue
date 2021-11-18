cf9e1ca553f661ebccc53c823ae94eee9420f1ec
whskrdns <whskrdns@ubware.com>
2021-11-18 14:21:57 +0900

    add layout

---
+++ frontend/src/components/BlogContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-sheet id="scrolling-techniques-3">
+    <v-container style="height: 1000px"></v-container>
+  </v-sheet>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ frontend/src/components/BlogFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-footer color="primary lighten-2" padless>
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+        {{ new Date().getFullYear() }} — <strong>UBcare</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      links: ['블로그', '채용'],
+    };
+  },
+};
+</script>
+
+<style></style>
